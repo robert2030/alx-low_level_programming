722f5dd2a34ac43ae34d013c7f72b4fe15c561e3
robert2030 <robert2030>
2023-08-14 11:27:47 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,24 +5,21 @@
  * Return: Alway return 0 (Success)
  */
 
-int main(void)
-{
-        char c;
-	char G;
+int main(void) {
+    char c;
+    char G;
 
-	G = 'A';
-        c = 'a';
+    G = 'A';
+    c = 'a';
 
-        while
-		(c <= 'z'){
-                        putchar(c);
-                                c++;
-                }
-	while
-		(G <= 'Z'){
-			putchar(G);
-			G++;
-		}
-        putchar('\n');
-        return (0);
+    while (c <= 'z') {
+      putchar(c);
+      c++;
+    }
+    while (G <= 'Z') {
+      putchar(G);
+      G++;
+    }
+    putchar('\n');
+    return (0);
 }
