126d841af4d2eaf7cd1d9a377638ae987150c96f
robert2030 <robert2030>
2023-08-14 11:19:17 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Alway return 0 (Success)
+ */
+
+int main(void)
+{
+        char c;
+	char G;
+
+	G = 'A';
+        c = 'a';
+
+        while
+                (c <= 'z')
+                {
+                        putchar(c);
+                                c++;
+                }
+	while
+		(G <= 'Z')
+		{
+			putchar(G);
+			G++;
+		}
+        putchar('\n');
+        return (0);
+}
