ae03b075ee96942868cc99919fc02000ecb44b6f
robert2030 <robert7ashioya.com>
2024-01-15 09:00:07 -0800

    all

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * Subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *modulas
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+
+int mod(int a, int b)
+{
+	return(a % b);
+}
